8480ef3f81e99c65a95a406054ab52341ea4a7a8
Reece Patten <rpatten1@css.edu>
2023-11-08 17:26:54 -0600

    Testing cont.

--- JavaScript/testing.js
+++ JavaScript/testing.js
@@ -1,28 +1,17 @@
-// server.js
-const path = require('path');
-const express = require('express');
-const app = express();
-const http = require('http').Server(app);
-const { Server } = require("socket.io");
-const io = new Server(http);
+// Note: this path assumes you've served socket.io properly on the server side
+var socket = io.connect('http://localhost:5500');
 
+function sendMessage() {
+  var message = document.getElementById('m').value;
+  socket.emit('chat message', message);
+  document.getElementById('m').value = '';
+  return false;
+}
 
-// Serve your static files (e.g., HTML, JS, CSS)
-app.use('/HTML', express.static(path.join(__dirname, 'HTML')));
-
-  
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  
-  socket.on('test', (message) => {
-    console.log(message);
-  });
-
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
-});
-
-http.listen(5500, () => {
-  console.log('listening on *:5500');
+// This function is to append the incoming messages to the 'messages' list
+socket.on('chat message', function(msg){
+  var item = document.createElement('li');
+  item.textContent = msg;
+  document.getElementById('messages').appendChild(item);
+  window.scrollTo(0, document.body.scrollHeight);
 });
